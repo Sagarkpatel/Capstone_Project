8824af25f0b12b4b86f82ed20bcd3695bdc3a8d9
Sagarkpatel <skp327@nyu.edu>
2021-11-01 20:30:59 -0400

    Updates

--- back_sub.py
+++ back_sub.py
@@ -1,55 +1,103 @@
 import numpy as np
 import cv2 as cv
 import pandas as pd
+import os
 
-path = "/Users/sagar/Desktop/Capstone/capstone_dataset_20210922/sub-NC232/ses-20190430/func/sub-NC232_ses-20190430_task-TASK_acq-normal_run-01_bold_video_cropped.mp4"
-cap = cv.VideoCapture(path)
 
-#pick a background subtractor method (MOG2 or GMG)
-fgbg=cv.bgsegm.createBackgroundSubtractorMOG()
+def getFilePaths():
+    #Use whatever directory data file was unzipped into
+    par_directory = "/Users/sagar/Desktop/Capstone/capstone_dataset_20210922"
 
-#initialize first frame
-ret, init_frame = cap.read()
-init = fgbg.apply(init_frame)
+    #Dictionary to hold file paths for video data for later processing (.mp4 files)
+    video_dict = {}
 
-#Store background subtraction differences in a dictionary
-diff_dict = {}
-target_times = list(np.arange(0,1.3*324,1.3))
+    #Drill down into each subjects data files
+    for sub_file in os.listdir(par_directory):
+        subject = sub_file
+        ses_directory = par_directory+"/" + str(sub_file)
+        if not sub_file.startswith('.'):
+            for ses_file in os.listdir(ses_directory):
+                if not ses_file.startswith('.'):
+                    root_dir = ses_directory+ "/" + ses_file+ "/func"
+                    for file in os.listdir(root_dir):
+                        if not file.startswith('.'):
+                            if file.endswith(".mp4"):
+                                par_file_dir = root_dir + "/"+ file
+                                video_dict[subject] = par_file_dir
+    return video_dict
 
-#Get fps and frame to calculate time
-fps = int(cap.get(cv.CAP_PROP_FPS))
-frame_count = 0
-time = 0 
+def getTargetTime(time):
+        target_times = list(np.arange(0,1.3*324,1.3))
+        return target_times[min(range(len(target_times)), key = lambda i: abs(target_times[i]-time))]
 
-while True:
-    print(time)
-    ret, frame = cap.read()
-    if frame is None:
-        break
-    
-    #Apply background subtraction mask to each frame
-    next = fgbg.apply(frame)
 
-    #Calculate differences in each frame
-    diff = next-init
-    sum_diff = diff.sum()
+video_dict = getFilePaths()
+for sub in video_dict:
 
-    #Store values
-    time = float(frame_count)/fps
-    diff_dict[time] = sum_diff
+    path = video_dict[sub]
+    cap = cv.VideoCapture(path)
 
-    #Display t-1 frame, t frame, and difference between two frames
-    # cv.imshow('Comparison', np.hstack((init, next, diff)))
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+    #pick a background subtractor method (MOG2 or GMG)
+    fgbg=cv.bgsegm.createBackgroundSubtractorMOG()
 
-    #Iterate
-    init = next
-    frame_count= frame_count + 1
+    #initialize first frame
+    ret, init_frame = cap.read()
+    init = fgbg.apply(init_frame)
 
-#Save data to intermediate file
-diff_pd = pd.DataFrame.from_dict(diff_dict,orient = "index")
-diff_pd.to_csv('back_sub.csv')
+    #Store background subtraction differences in a dictionary
+    # df = pd.DataFrame(columns=list(range(141050)))
+    df_total = pd.DataFrame(columns=["Difference"])
+    df_dense = pd.DataFrame(columns=list(range(141050)),dtype=float)
+    #Get fps and frame to calculate time
+    fps = int(cap.get(cv.CAP_PROP_FPS))
+    frame_count = 0
+    time = 0 
 
-cap.release()
-cv.destroyAllWindows()
+    while True:
+        ret, frame = cap.read()
+        if frame is None:
+            break
+        
+        #Apply background subtraction mask to each frame
+        next = fgbg.apply(frame)
+
+        #Calculate differences in each frame
+        diff = next-init
+
+        #Store values (each pixel or aggregative)
+        time = float(frame_count)/fps
+        time_proc = getTargetTime(time)
+        # df.loc[time] = diff.flatten()
+        # df_total.loc[time_proc] = np.abs(diff).sum()
+        df_dense.loc[time_proc] = list(diff.flatten())
+        #Display t-1 frame, t frame, and difference between two frames
+        #No need to display video
+
+        # cv.imshow('Comparison', np.hstack((frame, init, diff)))
+        # cv.imshow("Frame", frame)s
+        # cv.imshow("Difference", diff)
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        #Iterate
+        init = next
+        frame_count= frame_count + 1
+
+    #Save data to intermediate file
+    # print(f"Aggregating summed {sub}")
+    # df_total_diff = df_total.groupby(df_total.index).mean()
+    # print(f"Saving summed {sub}")
+    # df_total_diff.to_csv(f"data/total/{sub}.csv")
+
+
+    print(f"Aggregating dense {sub}")
+    df_dense_avg = df_dense.groupby(df_dense.index).mean()
+    print(f"Saving dense {sub}")
+    df_dense_avg.to_csv(f"data/dense/{sub}.csv")
+
+
+    #Done with subject
+    cap.release()
+    cv.destroyAllWindows()
+    print(f"Finished with {sub}")
